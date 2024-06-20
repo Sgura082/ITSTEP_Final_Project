69d1fc2be43db963c88006cbc7ad2ad5b130450d
Saba gurashvili <Sgura082@gmail.com>
2024-06-20 21:27:16 +0400

    Added interactive map of georgia(with empty links

--- FinalProject/Hello_Georgia/urls.py
+++ FinalProject/Hello_Georgia/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Hello_Georgia.urls'))
+    path('', views.home, name="Home"),
+    path('about/', views.home, name="Home"),
+
 ]
--- FinalProject/Hello_Georgia/views.py
+++ FinalProject/Hello_Georgia/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from faker import Faker
 
 # Create your views here.
+
+fake = Faker()
+
+posts = [
+    {"id": 1, "title": "Kahketi", 'description': fake.text(500)},
+    {"id": 2, "title": "Imereti", 'description': fake.text(500)},
+    {"id": 3, "title": "Guria", 'description': fake.text(500)}
+]
+
+#https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Georgia.svg/1920px-Georgia.svg.png
+def home(request):
+    return render(request, 'Home.html',{"posts": posts})
+
