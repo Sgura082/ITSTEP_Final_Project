ead7603bf734fce85a3cd7a289b81f43a2635175
Saba gurashvili <Sgura082@gmail.com>
2024-06-22 13:07:24 +0400

    finished decorating home page, and added ORM functionell for sql base

--- FinalProject/FinalProject/settings.py
+++ FinalProject/FinalProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Hello_Georgia.apps.HelloGeorgiaConfig'
+    'Hello_Georgia.apps.HelloGeorgiaConfig',
+
 ]
 
 MIDDLEWARE = [
--- FinalProject/Hello_Georgia/models.py
+++ FinalProject/Hello_Georgia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Visitor(models.Model):
+    visitor_email = models.EmailField
+    visitor_age = models.IntegerField
+    visitor_ID = models.CharField(max_length=11)
+    visitor_name = models.CharField(max_length=30)
+    visitor_last_name = models.CharField(max_length=30)
+    date_of_visit = models.DateField
+    pass
